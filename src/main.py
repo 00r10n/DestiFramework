1c1aa43ff52f76315be70b1078e08e18b513de80
Gerrit Stricker <gj.stricker@t-online.de>
2026-04-13 16:19:36 +0200

    Aufräumen

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from readData import readT6
 
 def main():
     # Reads T6 as np array from specified path. Format = Density, %vol, %mas, gA/100ml
-    t6 = readT6("./tables_raw/t6_nell.ods")
+    t6 = readT6("../tables_raw/t6_nell.ods")
     print(np.shape(t6))
     print(t6[:5, :])
 
