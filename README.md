a353dac571fa7e7e9c3f695e7340db779b9b31f4
Gerrit Stricker <gj.stricker@t-online.de>
2026-04-13 12:37:57 +0200

    clean up raw data and read tata into np array

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DestiFramework
 A collection of functions to perform mathematical operations often needed in a destillery
+
+
+
+# Notes: 
+- np.interp macht alles was ich brauche (interpolations weise)
+- datenimport mit pandas engine='odf'
---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from readData import readT6
+
+
+def main():
+    # Reads T6 as np array from specified path. Format = Density, %vol, %mas, gA/100ml
+    t6 = readT6("./tables_raw/t6_nell.ods")
+    print(np.shape(t6))
+    print(t6[:5, :])
+
+
+main()
