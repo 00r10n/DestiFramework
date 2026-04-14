9baa4184e0424e133a08d35980345c079b4e86f6
Gerrit Stricker <gj.stricker@t-online.de>
2026-04-14 10:08:46 +0200

    add input loop to main, test vor interp is wrong, has to be fixed

--- src/interpolation.py
+++ src/interpolation.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 
-
 def interp(array, inrow, outrow, value):
-    if value in array[inrow, :]:
-        index = np.where(array[inrow, :] == value)
-        return array[outrow, index]
-    upper_index = np.searchsorted(array[inrow, :], value)
+    if value in array[:, inrow]:
+        index = np.where(array[:, inrow] == value)
+        return array[index, outrow]
+    upper_index = np.searchsorted(array[:, inrow], value)
     lower_index = upper_index - 1
-    upper_x = array[inrow, upper_index]
-    lower_x = array[inrow, lower_index]
+    upper_x = array[upper_index, inrow]
+    lower_x = array[lower_index, inrow]
     fract = (value - lower_x) / (upper_x - lower_x)
-    upper_y = array[outrow, upper_index]
-    lower_y = array[outrow, lower_index]
+    upper_y = array[upper_index, outrow]
+    lower_y = array[lower_index, outrow]
     y = lower_y + (upper_y - lower_y) * fract
     return y
--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 import numpy as np
 
+from src.interpolation import interp
 from src.readData import readTable
 
 
 def main():
     # Reads T6 as np array from specified path. Format = Density, %vol, %mas, gA/100ml
     t6 = readTable("./tables_raw/t6_nell.ods")
+    print("Desti helper:\n input datatype, value, output Datatype\n\n")
+    while True:
+        print("What data is provided?\n0 = Density\n1 = %Vol\n2 = %mas\n3 = gA/100ml\n")
+        inrow = int(input())
+        print("provided value:")
+        value = float(input())
+        print("Provide expected ouput Datatype (see input)")
+        outrow = int(input())
+        retval = interp(t6, inrow, outrow, value)
+        print(f"interpolated Value:  {retval}\n")
+        print("n for next, x for exit")
+        cmd = input()
+        if cmd == "x":
+            break
 
 
 main()
